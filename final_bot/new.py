9a1097a10aed1507a8d9ef45de8d97c362cb4838
Chat_Bot <142486511+Databot2023@users.noreply.github.com>
2023-09-24 23:39:57 +0530

    Update new.py

--- final_bot/new.py
+++ final_bot/new.py
@@ -1,7 +1,11 @@
+# Importing the required modules for the program working
 import gradio as gr
 import openai
 from train import messages
+
+# Putting the API Key for nlp processing
 openai.api_key = "PASTE_THE_API_KEY"
+
 
 
 # Define a function to interact with the ChatGPT model
@@ -15,19 +19,20 @@ def CustomChatGPT(user_input):
     messages.append({"role": "assistant", "content": ChatGPT_reply})
     return ChatGPT_reply
 
-  
+# This part is for showing the chats on the chatting box and again doing it by appending the chats
 def respond(message, chat_history):
     bot_message = CustomChatGPT(message)
     chat_history.append((message, bot_message))
     return "", chat_history
 
 # Create a Gradio chatbot interface
-with gr.Blocks() as demo:
+with gr.Blocks(title="InfoMe",css=".gradio-container {background-color: #36454F}",theme=gr.themes.Default()) as demo:
     chatbot = gr.Chatbot()
-    msg = gr.Textbox()
+    msg = gr.Textbox(placeholder="Enter Your Query.....")
     clear = gr.ClearButton([msg, chatbot])
 
     # Connect the chatbot's input to CustomChatGPT
     msg.submit(respond, [msg, chatbot],[msg, chatbot])
 
+# Launching to the Web Browser(Now Demo)
 demo.launch()
